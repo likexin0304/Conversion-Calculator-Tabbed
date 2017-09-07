7882e2b8aaa9247ea54f98dc62be2e701793aca5
Shaowei Zhou <weizsw888@gmail.com>
2017-09-07 13:22:39 -0500

    Initial Commit

---
+++ Converstion Calculator Tabbed/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Converstion Calculator Tabbed
+//
+//  Created by weizsw on 9/7/17.
+//  Copyright © 2017 Shaowei Zhou. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
